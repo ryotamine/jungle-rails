25b7e7195eb53c9536ad803c6604a870337fc832
Ryota Mine <ryotamine@gmail.com>
2019-04-19 22:01:53 +0000

    make POST request for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :users
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create]
+  end
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
