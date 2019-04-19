21bc9b39810c5d431830c1a970d88f45b87165d0
Ryota Mine <ryotamine@gmail.com>
2019-04-19 19:29:20 +0000

    add HTTP basic authentication for Admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: 'username', password: 'password'
+  
   def index
     @categories = Category.order(id: :desc).all
   end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: 'username', password: 'password'
+
   def show
   end
+  
 end
