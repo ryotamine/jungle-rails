5ee77fc85716c4202866218dc1d1c36af468129f
Ryota Mine <ryotamine@gmail.com>
2019-04-22 22:12:03 +0000

    clean up code

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -35,39 +35,4 @@ RSpec.describe User, :type => :model do
       expect(user).to_not be_valid
     end
   end
-
-  # describe ".authenticate_with_credentials" do
-  #   user = User.new(
-  #     password: "test", password_confirmation: "test", email: "test@test.com", first_name: "test", last_name: "test"
-  #   )
-
-  #   it "is valid with valid attributes" do
-  #     expect(user).to be_valid
-  #   end
-
-  #   it "is not valid without a password" do
-  #     user.password = nil
-  #     expect(user).to_not be_valid
-  #   end
-
-  #   it "is not valid without a password_confirmation" do
-  #     user.password_confirmation = nil
-  #     expect(user).to_not be_valid
-  #   end
-
-  #   it "is not valid without an email" do
-  #     user.email = nil
-  #     expect(user).to_not be_valid
-  #   end
-
-  #   it "is not valid without a first_name" do
-  #     user.first_name = nil
-  #     expect(user).to_not be_valid
-  #   end
-
-  #   it "is not valid without a last_name" do
-  #     user.last_name = nil
-  #     expect(user).to_not be_valid
-  #   end
-  # end
 end
